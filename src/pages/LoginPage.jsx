1d06b4ab16b871ca577342fc1dedffb7d9f88aa5
Asia <joanna.falkowska93@gmail.com>
2026-03-25 16:03:07 +0100

    Setup basic login form

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,10 +1,59 @@
+import {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+import {supabase} from '../lib/supabase';
+
 export default function LoginPage() {
+	const navigate = useNavigate();
+	const [isLoading, setIsLoading] = useState(false);
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	const handleLogin = async(event) => {
+		event.preventDefault();
+		setIsLoading(true);
+		const {error} = await supabase.auth.signInWithPassword({
+			email,
+			password,
+		});
+		if (error) {
+			alert(error.error_description || error.message);
+			setIsLoading(false);
+		}
+		navigate('/events');
+	};
+
 	return (
 		<div className="min-h-screen flex items-center justify-center bg-slate-100 p-6">
 			<div className="w-full max-w-md rounded-2xl bg-white p-8 shadow">
 				<h1 className="text-2xl font-bold">Zaloguj się</h1>
-				<p className="mt-2 text-sm text-slate-600">
-				</p>
+				<form className="flex flex-col" onSubmit={handleLogin}>
+					<label htmlFor="email">Email:</label>
+					<input
+						id="email"
+						type="email"
+						placeholder="Twój email"
+						value={email}
+						required={true}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+					<label htmlFor="password">Hasło:</label>
+					<input
+						id="password"
+						type="password"
+						placeholder="************"
+						value={password}
+						required={true}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+
+					<button
+						disabled={isLoading}
+					>
+						{isLoading
+							? <span>Ładowanie</span>
+							: <span>Potwierdź</span>}
+					</button>
+				</form>
 			</div>
 		</div>
 	);
