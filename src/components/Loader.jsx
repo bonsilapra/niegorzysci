ad96cf9ff14df3a0056ac23c1f682a8de79ecdad
Asia <joanna.falkowska93@gmail.com>
2026-04-01 14:43:34 +0200

    Improve loading page and adjust `Loader`

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,14 @@
-export const Loader = () => {
-	return (
-		<span class="loader"></span>
-	);
+export const Loader = ({type = 'button'}) => {
+	if (type === 'button') {
+		return (
+			<span className="loader"></span>
+		);
+	}
+	if (type === 'full-page') {
+		return (
+			<div className="flex items-center h-50">
+				<span className="loader loader-dark"></span>
+			</div>
+		);
+	}
 };
--- src/components/routeGuards/AuthBootstrap.jsx
+++ src/components/routeGuards/AuthBootstrap.jsx
@@ -1,11 +1,12 @@
 import {Outlet} from 'react-router';
 import {useAuth} from '../../context/AppContext';
+import {Loader} from '../Loader';
 
 export function AuthBootstrap() {
 	const {isReady} = useAuth();
 
 	if (!isReady) {
-		return <div>Loading...</div>;
+		return <Loader type="full-page" />;
 	}
 
 	return <Outlet />;
