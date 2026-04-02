a01c8f2335e903a6f9fa32c69aeacc66b8f8cf6e
Asia <joanna.falkowska93@gmail.com>
2026-04-02 10:26:59 +0200

    Adjust layout

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -6,18 +6,18 @@ export const Layout = ({children}) => {
 	const {isLoggedIn, isApproved} = useAuth();
 
 	const backgroundStyle = isLoggedIn && isApproved
-		? 'bg-secondary-200'
+		? 'bg-linear-to-b from-primary-0 to-primary-300'
 		: 'bg-[url(/img/background.webp)]';
 
-	const beforeLoginContainer = 'w-2/3 max-w-md rounded-2xl bg-primary-0 flex flex-col p-6 shadow-xl text-primary-600 text-xl';
-	const afterLoginContainer = 'w-3/4 max-w-md';
+	const beforeLoginContainer = 'w-full md:w-2/3 max-w-md rounded-2xl bg-primary-0 flex flex-col p-6 shadow-xl text-primary-600 text-xl';
+	const afterLoginContainer = 'w-3/4 max-w-md p-6';
 
 	return (
 		<div className={`${backgroundStyle} flex flex-col min-h-screen bg-cover bg-center`}>
 			{isLoggedIn &&
 				<Navbar />
 			}
-			<main className="flex flex-1 flex-col items-center justify-center">
+			<main className="flex flex-1 flex-col items-center justify-center px-4">
 				<div className={cx('text-primary-600 text-xl', {
 					[afterLoginContainer]: isLoggedIn && isApproved,
 					[beforeLoginContainer]: !isLoggedIn || !isApproved,
