ae77248ebf2e10dc6994a3ae51bcd2e1a60ed588
Asia <joanna.falkowska93@gmail.com>
2026-04-09 11:36:28 +0200

    Use `draft` name iinstead of `event`

--- src/pages/AdminEventFormPage.jsx
+++ src/pages/AdminEventFormPage.jsx
@@ -6,19 +6,18 @@ import {Button} from '../components/Button';
 export default function AdminEventFormPage() {
 	const {id} = useParams();
 	const [isLoading, setIsLoading] = useState(false);
-	const [event, setEvent] = useState({
+	const [draft, setDraft] = useState({
 		title: '',
 		begin: '',
 		end: '',
 		content: '',
-		status: 'draft',
 	});
 
 	const title = id ? 'Edytuj rajd' : 'Dodaj rajd';
 
 	const handleChange = (event) => {
 		const {name, value} = event.target;
-		setEvent((prev) => ({
+		setDraft((prev) => ({
 			...prev,
 			[name]: value,
 		}));
@@ -31,43 +30,49 @@ export default function AdminEventFormPage() {
 	return (
 		<>
 			<h1 className="text-2xl font-bold mb-5">{title}</h1>
-			<form className="rounded-2xl bg-primary-0 p-6 shadow-xl flex flex-col" onSubmit={handleSave}>
-				<div className="grid grid-cols-2 gap-x-5">
-					{eventItems.map((item) =>
-						<UserInput
-							label={item.label}
-							id={item.id}
-							key={item.id}
-							name={item.name}
-							type={item.type || 'text'}
-							placeholder={item.placeholder}
-							value={event[item.name]}
-							required={true}
-							onChange={handleChange}
-							isDisabled={isLoading}
-							cssLayout={item.type === 'date' ? 'col-span-full sm:col-span-1' : 'col-span-full'}
-						/>,
-					)}
-				</div>
-				<div className="grid grid-cols-2 gap-x-5 gap-y-5 sm:gap-y-0 justify-items-center">
-					<Button
-						content="Zapisz szkic"
-						type="secondary"
-						isLoading={isLoading}
-						cssClass="sm:w-50! col-span-full sm:col-span-1"
-					/>
-					<Button
-						content="Opublikuj"
-						isLoading={isLoading}
-						cssClass="sm:w-50! col-span-full sm:col-span-1"
-					/>
-				</div>
-			</form>
+			<div className="rounded-2xl bg-primary-0 p-6 shadow-xl flex flex-col">
+				<form onSubmit={handleSave}>
+					<div className="grid grid-cols-2 gap-x-5">
+						{draftItems.map((item) =>
+							<UserInput
+								label={item.label}
+								id={item.id}
+								key={item.id}
+								name={item.name}
+								type={item.type || 'text'}
+								placeholder={item.placeholder}
+								value={draft[item.name]}
+								required={true}
+								onChange={handleChange}
+								isDisabled={isLoading}
+								cssLayout={item.type === 'date' ? 'col-span-full sm:col-span-1' : 'col-span-full'}
+							/>,
+						)}
+					</div>
+					<div className="grid grid-cols-2 gap-x-5 gap-y-5 sm:gap-y-0 justify-items-center">
+						<Button
+							content="Zapisz szkic"
+							type="secondary"
+							isLoading={isLoading}
+							cssClass="sm:w-50! col-span-full sm:col-span-1"
+						/>
+						<Button
+							content="Opublikuj"
+							isLoading={isLoading}
+							cssClass="sm:w-50! col-span-full sm:col-span-1"
+						/>
+					</div>
+				</form>
+				<Button
+					path="/admin/events/drafts"
+					simple
+				/>
+			</div>
 		</>
 	);
 }
 
-const eventItems = [
+const draftItems = [
 	{
 		label: 'Tytuł:',
 		id: 'title',
