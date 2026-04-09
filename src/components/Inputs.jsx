459fcf792efa48da5686b879f20f9a1ede78c8e8
Asia <joanna.falkowska93@gmail.com>
2026-04-09 10:13:30 +0200

    Add textarea option to inputs and modify layout

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -11,36 +11,54 @@ export const UserInput = ({
 	onChange,
 	isDisabled,
 	cssClass,
+	cssLayout,
 }) => {
 	const inputDefault = 'border-primary-300 bg-white';
 	const inputFocus = 'focus:border-primary-500 focus:bg-primary-100';
 	const inputActive = 'active:border-primary-600 active:bg-primary-200';
 
 	return (
-		<>
+		<div className={cx('flex flex-col', cssLayout)}>
 			<label
 				htmlFor={id}
 				className="mb-2"
 			>
 				{label}
 			</label>
-			<input
-				id={id}
-				type={type}
-				name={name}
-				placeholder={placeholder}
-				className={cx('p-2 mb-5 border-2 rounded-md',
-					inputDefault,
-					inputFocus,
-					inputActive,
-					{'bg-primary-100! text-primary-400!': isDisabled},
-					cssClass,
-				)}
-				value={value}
-				required={isRequired}
-				onChange={onChange}
-				disabled={isDisabled}
-			/>
-		</>
+			{type === 'textarea'
+				? <textarea
+					id={id}
+					name={name}
+					placeholder={placeholder}
+					className={cx('p-2 mb-5 border-2 rounded-md',
+						inputDefault,
+						inputFocus,
+						inputActive,
+						{'bg-primary-100! text-primary-400!': isDisabled},
+						cssClass,
+					)}
+					value={value}
+					required={isRequired}
+					onChange={onChange}
+				/>
+				: <input
+					id={id}
+					type={type}
+					name={name}
+					placeholder={placeholder}
+					className={cx('p-2 mb-5 border-2 rounded-md',
+						inputDefault,
+						inputFocus,
+						inputActive,
+						{'bg-primary-100! text-primary-400!': isDisabled},
+						cssClass,
+					)}
+					value={value}
+					required={isRequired}
+					onChange={onChange}
+					disabled={isDisabled}
+				/>
+			}
+		</div>
 	);
 };
